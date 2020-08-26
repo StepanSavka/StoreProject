7f18b01581ab6bdbf5c17a9f9b125c8b14644293
stepansavka <stepansavka@protonmail.com>
2020-08-26 13:43:39 +0700

    header_modify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/font-awesome/font-awesome.css')}}">
 
     @yield('custom-title')
     @yield('custom_styles')
@@ -40,24 +41,30 @@
         .btn-success{
             background-color: #28E8DB;
         }
+        .mr-header{
+            margin-right: 26%;
+        }
+        .dropdown-menu{
+            min-width: 5rem;
+        }
     </style>
 </head>
 <body>
 <div class="body">
     <div class="d-flex justify-content-center p-1 bg-navbar">
-        <a class="btn pl-auto pr-auto bd-highlight text-white">最新消息</a>
-        <a class="btn pl-auto pr-auto bd-highlight text-white">新型肺炎疫情最新安排</a>
-        <a class="btn btn-latest-news pl-auto pr-auto bd-highlight text-current" href="javascript:void(0)">查看</a>
+        <a href="javascript:void(0)" class="btn pl-auto pr-auto bd-highlight text-white">最新消息</a>
+        <a href="javascript:void(0)" class="btn pl-auto pr-auto bd-highlight text-white">新型肺炎疫情最新安排</a>
+        <a href="javascript:void(0)" class="btn btn-latest-news pl-auto pr-auto bd-highlight text-current" href="javascript:void(0)">查看</a>
     </div>
     @include('layouts.header')
     @yield('custom_content')
 </div>
+<script src="{{asset('js/jquery-3.5.1.slim.min.js')}}" ></script>
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}" ></script>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 @yield('custom_scripts')
 </body>
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,62 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hip
- * Date: 8/25/2020
- * Time: 10:22 PM
- */
+
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light nav-justified">
+        <a class="navbar-brand" href="javascript:void(0)" >
+            <img src="{{asset('index/images/logo@2x.png')}}" width="60" height="30" alt="logo@2x.png" loading="lazy">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse navbar-right" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)"><nobr>關於我們</nobr></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)"><nobr>最新資訊</nobr></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <nobr>分店位置</nobr>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item dropdown-toggle" href="javascript:void(0)">新界區</li>
+                        <li class="dropdown-item dropdown-toggle" href="javascript:void(0)">九龍區</li>
+                        <li class="dropdown-item dropdown-toggle" href="javascript:void(0)">香港島</li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)"><nobr>空間計算器</nobr></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)"><nobr>常見問題</nobr></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)"><nobr>聯絡我們</nobr></a>
+                </li>
+            </ul>
+            <ul class="navbar-nav" style="margin-left: 3%;">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)" >
+                        <img src="{{asset('index/images/Component 7 – 1@2x.png')}}">
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav" style="margin-left: 3%;">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">
+                        <img src="{{asset('index/images/icons8-user-50@2x.png')}}">
+                        <span>登入</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">
+                        <img src="{{asset('index/images/icons8-join-50@2x.png')}}">
+                        <span>註冊</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
